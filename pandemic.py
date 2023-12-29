aecf7b2ea699ba7b192a98bd7a4a32f191bd3efa
Dan Z <dzauski585@gmail.com>
2023-12-28 20:48:16 -0500

    comments

--- pandemic.py
+++ pandemic.py
@@ -26,14 +26,23 @@ E0 = 0 #exposed
 I0 = 1 #infected
 R0 = 0.00  # recovered
 D0 = 0 #dead
-S0 = (0.99 * N) - I0 - R0  #99% susceptible minus the infected and recovered
+S0 = (0.9999 * N) - I0 - R0  #99% susceptible minus the infected and recovered
 y0 = [S0, E0, I0, R0, D0]
 
 # Parameters
-beta = 8  #contact rate 
-sigma = 0.1   #incubation
-gamma = 0.05  #mean recovery rate
-mu = 1 #death rate
+beta = 1  #contact rate or infection rate each interaction will infect for example 0.25 is 1 in 4 interactions infect
+# beta = c*p where c = average number of contacts a susceptible person makes per time and p = probability that susceptable people become infected with contact to infected person
+
+'''
+lambda = beta * (I / N) or the rate of movement from exposed to infected
+'''
+sigma = 0.5   #incubation
+gamma = 0.4  #mean recovery rate in 1 over days
+mu = 0.01 #death rate technically mu is normal death rate of population and disease mu is (mu / mu + beta)
+
+Rnaught = beta / gamma
+
+critical = 1 - (1 / Rnaught)
 
 # Time vector
 t = np.linspace(0, 200, 200)  
@@ -44,6 +53,10 @@ solution = odeint(SEIR_model, y0, t, args=(N, beta, sigma, gamma, mu))
 # Extract results
 S, E, I, R, D = solution.T
 
+# Print the Rnaught and critical threshhold
+print(f"The Rnaught is {Rnaught}")
+print(f"The critical vaccination or herd immunity threshold is {critical}")
+
 # Plot the results
 plt.figure(figsize=(10, 6))
 plt.plot(t, S, label='Susceptible')
@@ -53,7 +66,7 @@ plt.plot(t, R, label='Recovered')
 plt.plot(t, D, label='Dead')
 plt.xlabel('Time (days)')
 plt.ylabel('Population in Billions')
-plt.title('SEIR Model Simulation')
+plt.title('SEIRD Model Simulation')
 plt.legend()
 plt.grid(True)
 plt.show()
